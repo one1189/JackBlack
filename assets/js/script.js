b3b669ca846414c5aee4f1dccd71467d5fd12639
one1189 <>
2023-05-07 12:02:32 +0000

    enables jb to recieve more cards if total < 17

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 let jbSum = 0;
 let userSum = 0;
 
+let jbAceCount = 0;
+let userAceCound = 0;
+
 let hidden;
+let deck;
+
+let canTwist = true; //Allows user to take another card if total value <= 21
 
 window.onload = function () {
     createDeck();
@@ -37,11 +43,24 @@ function shuffleDeck() {
 function beginGame() {
     hidden = deck.pop(); //removes a card from the deck array and allocates to hidden jb card
     jbSum += getValue(hidden);
+    jbAceCount += checkAce(hidden);
+    // console.log(hidden);
+    // console.log(jbSum);
+
+    while (jbSum < 17) { //Allows jb to take another card if total < 17
+        let allowCard = document.createElement("img");
+        let card = deck.pop();
+        allowCard.src = "./images/cards/" + card + ".png";
+        jbSum += getValue(card);
+        jbAceCount += checkAce(card);
+        document.getElementById("jbhand").append(allowCard);
+    }
+    console.log(jbSum);
 }
 
 function getValue(card) { //finds the value of the card and assigns "A" the value of 11 and "K, Q, J" the value of 10 
     let data = card.split("-");
-    let value = date[0];
+    let value = data[0];
 
     if (isNaN(value)) {
         if (value == "A") {
@@ -50,4 +69,11 @@ function getValue(card) { //finds the value of the card and assigns "A" the valu
         return 10;
     }
     return parseInt(value);
+}
+
+function checkAce(card) {
+    if (card[0] == "A") {
+        return 1;
+    }
+    return 0;
 }
