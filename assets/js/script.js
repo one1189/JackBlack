15c8c4884fc4908e11454b7a5ad7a92a4f22af76
one1189 <>
2023-05-07 10:56:23 +0000

    create a 52 card deck

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,14 @@ window.onload = function () {
 function createDeck() {
     let cardValue = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     let cardSuit = ["C", "D", "H", "S"];
+
+    deck = [];
+
+    for (let i = 0; i < cardSuit.length; i++) {
+        for (let j = 0; j < cardValue.length; j++) {
+            deck.push(cardValue[j] + "-" + cardSuit[i]);
+        }
+    }
+
+    console.log(deck);
 }
