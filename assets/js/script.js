8af16957c9581a204dbec14a52df6aa571a4a9f2
one1189 <>
2023-05-07 10:36:28 +0000

    begin JavaScript coding

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let jbSum = 0;
+let userSum = 0;
+
+window.onload = function () {
+    createDeck();
+};
+
+function createDeck() {
+    let cardValue = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let cardSuit = ["C", "D", "H", "S"];
+}
