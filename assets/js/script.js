4ede9d35cfd8f48dab3777b790d584031a93d7fc
one1189 <>
2023-05-11 08:22:58 +0000

    create openGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 let jbSum = 0;
 let userSum = 0;
 
+
+
+
+//let userSumDisplay = document.getElementsByClassName("usertotal");
+
 let jbAceCount = 0;
 let userAceCount = 0;
 
@@ -113,9 +118,6 @@ function stick() {
         alert(`Loser!! Jack scored ${jbSum}, loser scores ${userSum}.`);
     }
 
-    //document.getElementsByClassName("jbtotal").innerText = jbSum;
-    //document.getElementsByClassName("usertotal").innerText = userSum;
-
     /*Create a function to allow user to start a new game 2 seconds after
     witnessing the result*/
     setTimeout(function () {
@@ -151,4 +153,24 @@ function reduceAce(userSum, userAceCount) {
         userAceCount -= 1;
     }
     return userSum;
+}
+
+/*function incrementScore() {
+    userSum += cardValue++;
+    userSumDisplay.textContent = usertotal;
+} */
+
+/* A funciton that opens the game only if a username is entered
+And alerts the user to enter a name if there is no value in the field */
+function openGame() {
+
+    let userName = document.getElementById("name").value;
+
+    if (userName === "") {
+        alert("Please enter your Name");
+    }
+    else {
+        alert(`Welcome to JackBlack ${userName}`)
+        window.location.href = "jackblack.html";
+    }
 }
