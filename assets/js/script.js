6a79981c2d7fa7244c7720750a3a71446a3afa30
one1189 <>
2023-05-11 12:13:37 +0000

    create sound checkbox for audio playing

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,12 @@ let deck;
 
 let canTwist = true; //Allows user to take another card if total value <= 21
 
+let isSoundOn = false;
+
+document.getElementById("toggle-sound").addEventListener("click", function() {
+  isSoundOn = true;
+});
+
 /* A funciton that opens the game only if a username is entered
 And alerts the user to enter a name if there is no value in the field */
 function openGame() {
@@ -121,28 +127,38 @@ function stick() {
 
     if (userSum > 21) {
         alert(`Bummer! You bust with ${userSum}.`);
+        if (isSoundOn) {
         let audioBust = new Audio("/assets/audio/come-on-a-yeah-hah.mp3");
         audioBust.play();
+        }
     }
     else if (jbSum > 21) {
         alert(`You Won!! Jack Black busted on ${jbSum}.`);
+        if (isSoundOn) {
         let audioWin = new Audio("/assets/audio/god-dammit-cage.mp3");
         audioWin.play();
+        }
     }
     else if (jbSum == userSum) {
         alert("It's a tie!!");
+        if (isSoundOn) {
         let audioTie = new Audio("/assets/audio/jack-black-no-way.mp3");
         audioTie.play();
+        }
     }
     else if (userSum > jbSum) {
         alert(`Winner!! You scored ${userSum}, whilst Jack scored ${jbSum}.`);
+        if (isSoundOn) {
         let audioWin2 = new Audio("/assets/audio/jack-black-come-on-with-order.mp3");
         audioWin2.play();
+        }
     }
     else if (userSum < jbSum) {
         alert(`Loser!! Jack scored ${jbSum}, loser scores ${userSum}.`);
+        if (isSoundOn) {
         let audioLose = new Audio("/assets/audio/dig-it-jack-black.mp3");
         audioLose.play();
+        }
     }
 
     document.querySelector(".jbtotal").innerText = jbSum;
